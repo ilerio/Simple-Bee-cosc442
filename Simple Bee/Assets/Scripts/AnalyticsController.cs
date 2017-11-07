35b4644eb91ad2370ac3e9aa3864f0d3d97f8a60
Ileri Oyedele <ilerioyedele@gmail.com>
2017-11-07 11:22:10 -0800

    Build_2

---
+++ Simple Bee/Assets/Scripts/AnalyticsController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+using UnityEngine.Analytics.Experimental;
+
+public class AnalyticsController : MonoBehaviour
+{
+
+    public string levelName;
+
+    // Use this for initialization
+    void Start()
+    {
+        AnalyticsEvent.LevelStart(levelName);
+        Debug.Log("Analytics Event Track: level start (" + levelName + ")");
+    }
+
+    public void EndLevel()
+    {
+        AnalyticsEvent.LevelComplete(levelName);
+        Debug.Log("Analytics Event Track: level end (" + levelName + ")");
+    }
+
+    public void QuestionStart(string questionName)
+    {
+        AnalyticsEvent.Custom("QuestionStart | " + questionName);
+        Debug.Log("Analytics Event Track: question start (" + questionName + ")");
+    }
+
+    public void QuestionEnd(string questionName)
+    {
+        AnalyticsEvent.Custom("QuestionEnd | " + questionName);
+        Debug.Log("Analytics Event Track: question end (" + questionName + ")");
+    }
+
+    public void DialougeStart()
+    {
+        AnalyticsEvent.Custom("DialougeStart");
+        Debug.Log("Analytics Event Track: dialouge start");
+    }
+
+    public void DialougeEnd()
+    {
+        AnalyticsEvent.Custom("DialougeEnd");
+        Debug.Log("Analytics Event Track: dialouge end");
+    }
+
+    public void BeeWin()
+    {
+        AnalyticsEvent.Custom("BeeWin");
+        Debug.Log("Analytics Event Track: Bee win");
+    }
+
+    public void BeeLose()
+    {
+        AnalyticsEvent.Custom("BeeLose");
+        Debug.Log("Analytics Event Track: Bee lose");
+    }
+}
---
+++ Simple Bee/Assets/Scripts/ForFungus/ExitScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class ExitScript : MonoBehaviour {
+
+    public Flowchart flowchart;
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        Debug.Log("Exited");
+        flowchart.SendFungusMessage("exit");
+    }
+}
